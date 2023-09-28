c85d2174a00e1b8414dd6233019b85cf3ac2d9ab
Alessandro Cavallotti <alessandro@junto.space>
2023-09-28 23:38:17 +0200

    Controlling play and pause

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+// Import our custom CSS
+import './scss/style.scss'
+// import * as bootstrap from 'bootstrap'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
