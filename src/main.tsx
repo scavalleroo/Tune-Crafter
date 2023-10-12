917dd04207a9d7ee7f4153f4cf42a140239b6d86
Alessandro Cavallotti <alessandro@junto.space>
2023-10-12 12:53:04 +0200

    Style

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-// Import our custom CSS
 import './scss/style.scss'
-// import * as bootstrap from 'bootstrap'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
