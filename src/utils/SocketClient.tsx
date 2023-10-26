241fae139c6000dcf8312e31fed775636039abce
Alessandro Cavallotti <alessandro@junto.space>
2023-10-26 10:05:46 +0200

    Updates

--- src/utils/SocketClient.tsx
+++
@@ -1,7 +0,0 @@
-import { io } from 'socket.io-client';
-
-const ipAddress = import.meta.env.VITE_IP_ADDRESS_SERVER_SOCKET;
-const port = import.meta.env.VITE_PORT_SERVER_SOCKET;
-
-let url = "http://" + ipAddress + ":" + port + "/";
-export const socket = io(url, {autoConnect: false});
